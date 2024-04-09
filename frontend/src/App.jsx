08f2fa19f280c98d7b3716ac0d7497a6412bd352
Emmanuel Andem <emmanuelandem360@gmail.com>
2024-04-09 11:02:17 +0100

    updated stack

---
+++ frontend/src/App.jsx
@@ -0,0 +1,16 @@
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home/Home";
+import Signup from "./pages/Signup/Signup";
+import Login from "./pages/Login/Login";
+
+function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/signup" element={<Signup />} />
+    </Routes>
+  );
+}
+
+export default App;
