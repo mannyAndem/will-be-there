b98ea530b9db7a24de9b780d2b597b8ebf4772d5
Emmanuel Andem <emmanuelandem360@gmail.com>
2024-04-10 09:42:55 +0100

    feat: create login and signup hooks

---
+++ frontend/src/hooks/auth.js
@@ -0,0 +1,50 @@
+/**
+ * File contains hooks to handle authentication
+ */
+
+import { useMutation } from "@tanstack/react-query";
+import axios from "../api/axios";
+import { useAuthContext } from "../contexts/AuthContext";
+import { useEffect } from "react";
+
+export const useLogin = () => {
+  const { setTokens, tokens } = useAuthContext();
+
+  console.log(tokens);
+  const { isError, isPending, isSuccess, data, error, mutate } = useMutation({
+    mutationFn: (data) => axios.post("auth/login", JSON.stringify(data)),
+  });
+
+  useEffect(() => {
+    if (isSuccess) {
+      console.log(data);
+      setTokens(data.data.token);
+    }
+    if (isError) {
+      console.error(error);
+    }
+  }, [isSuccess, isError]);
+
+  return { login: mutate, isSuccess, isError, isPending };
+};
+
+export const useSignup = () => {
+  const { setTokens, tokens } = useAuthContext();
+
+  console.log(tokens);
+  const { isError, isPending, isSuccess, data, error, mutate } = useMutation({
+    mutationFn: (data) => axios.post("auth/register", JSON.stringify(data)),
+  });
+
+  useEffect(() => {
+    if (isSuccess) {
+      console.log(data);
+      setTokens(data.data.token);
+    }
+    if (isError) {
+      console.error(error);
+    }
+  }, [isSuccess, isError]);
+
+  return { signup: mutate, isSuccess, isError, isPending };
+};
