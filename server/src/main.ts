6f5f2da70093c3ff50d31aab60385f4d6ce3111a
Olamide <simonadepetoye@gmail.com>
2024-04-15 19:52:54 +0100

    --empty

--- server/src/main.ts
+++ server/src/main.ts
@@ -7,9 +7,7 @@ import { AppModule } from './modules/app/app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors({
-    origin: 'http://localhost:3000',
-  });
+  app.enableCors();
 
   app.setGlobalPrefix('api');
   app.useGlobalPipes(
