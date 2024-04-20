01339e04bac654e363719608fa6d33fd90f7ee6a
Olamide <simonadepetoye@gmail.com>
2024-04-20 22:20:13 +0100

    --empty

--- server/src/modules/events/events.module.ts
+++ server/src/modules/events/events.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
 import { PrismaService } from 'src/prisma.service';
 import { EventsController } from './events.controller';
 import { EventsService } from './events.service';
 
 @Module({
   controllers: [EventsController],
-  providers: [EventsService, PrismaService],
+  providers: [EventsService, PrismaService, JwtService],
 })
 export class EventsModule {}
