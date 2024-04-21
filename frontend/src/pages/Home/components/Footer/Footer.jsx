16117613f31ecf8bd2ca4f2408494012d6412ae8
Godwin Daniel <lemuelgodwin2@gmail.com>
2024-04-21 08:46:54 +0100

    Footer editing

---
+++ frontend/src/pages/Home/components/Footer/Footer.jsx
@@ -0,0 +1,15 @@
+import "./footer.scss"
+const Footer = () => {
+    return (
+        <footer>
+        <div className="footerContents">
+            <div className="company">
+                <h1>WILL.BE.THERE</h1>
+                <h3>Lorem ipsum dolor sit amet consectetur. Lobortis lorem id nunc mi dictum parturient nisi. Lectus nunc eu velit volutpat facilisis eget ac. </h3>
+            </div>
+        </div>
+        </footer>
+      );
+}
+ 
+export default Footer;
